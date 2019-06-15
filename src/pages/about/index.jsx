38e604b3323012877d9a849fb17f2a7f767f459c
kinsliy <kinsliy@qq.com>
2019-06-15 17:53:03 +0800

    feat:优化代码

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -4,61 +4,57 @@ import './index.less';
 import { connect } from 'react-redux';
 import {Button} from 'antd';
 import types from '@/store/about/types.js'
-
+import {getList} from '@/fetch/about/index.js'
 function App (props){
 
 
-    // const plus=()=>{
+    const plus=()=>{
 
-    //     props.dispatch({
-    //         type:types.changeData,
-    //         data:{
-    //             type:'count',
-    //             value:props.count+1
-    //         },
+        props.dispatch({
+            type:types.changeData,
+            data:{
+                type:'count',
+                value:props.count+1
+            },
            
-    //     })
-    // }
-
-    // const fet=()=>{
-
-    //     props.dispatch({
-    //         type:types.getList,
-    //         data:{
-    //            a:1213,
-    //         }
-    //     })
-    // }
-
-    const [data, setCount] = useState({count:0});
-
-    const a = ()=>{
-        console.log(222)
-        data.count++
-        setCount({
-            count:data.count
         })
     }
 
-    // return(
-    //     <div className='about'>
-    //         关于页面{props.count}
-    //         <div>
-    //         <Button type="primary" >点击增加数字</Button>
-    //         </div>
-    //         <div>
-    //         <Button type="primary"  >发送请求</Button>
-    //         </div>
-    //     </div>
-    // )
-    return (
-              <div>
-                <p>You clicked {data.count} times</p>
-                <button onClick={a}>
-                Click me
-               </button>
+    const fet=()=>{
+        getList().then(data=>{
+            props.dispatch({
+                type:types.changeData,
+                data:{
+                    type:'count',
+                    value:78
+                }
+            })
+        })
+        
+    }
+
+    // const [data, setCount] = useState({count:0});
+
+    // const a = ()=>{
+    //     console.log(222)
+    //     data.count++
+    //     setCount({
+    //         count:data.count
+    //     })
+    // }
+
+    return(
+        <div className='about'>
+            关于页面{props.count}
+            <div>
+            <Button type="primary" onClick={fet}>点击增加数字</Button>
             </div>
-           );
+            <div>
+            <Button type="primary"  >发送请求</Button>
+            </div>
+        </div>
+    )
+  
 }
 
 
@@ -66,4 +62,4 @@ const mapStateToProps = (state) => ({
     count: state.about.count
 })
 
-export default App;
+export default connect(mapStateToProps)(App);
