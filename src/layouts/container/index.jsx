2c9385f5d8d7ebd37a2e29136fe87bb53aa5dcfa
kinsliy <kinsliy@qq.com>
2019-06-15 12:05:52 +0800

    feat:优化代码

--- src/layouts/container/index.jsx
+++ src/layouts/container/index.jsx
@@ -13,6 +13,25 @@ class SiderDemo extends React.Component {
   state = {
     collapsed: false,
     marginLeft:'256px',
+    menuList:[
+      { path:"/back/1",title:"meun1",icon:"book" },
+      { path:"/back/2",title:"meun2",icon:"issues-close",
+          children:[
+            { title:"meun3",path:"/back/3",icon:"info-circle",children:[
+                { title:"meun4",path:"/back/3/4",icon:"bars"},
+                { title:"meun5",path:"/back/3/5",icon:"bars"},
+                { title:"meun6",path:"/back/3/6",icon:"bars"},
+                { title:"meun7",path:"/back/3/7",icon:"bars" }
+              ] },
+            { title:"meun8",path:"/8",icon:"branches",children:[
+                { title:"meun9",path:"/9",icon:"bars" },
+                { title:"meun10",path:"/10",icon:"bars" },
+              ] },
+            { title:"meun11",path:"/11",icon:"bars" },
+            { title:"meun12",path:"/12",icon:"bars"},
+          ]
+       }
+    ],
   };
 
   toggle = () => {
@@ -38,7 +57,7 @@ class SiderDemo extends React.Component {
   render() {
     return (
       <Layout className='container' style={{ minHeight: '100vh' }} >
-          <SiderCommon  changeMargin={this.changeMargin} />
+          <SiderCommon  menulist={this.state.menuList}    changeMargin={this.changeMargin} />
 
           <Layout style={{ marginLeft: this.state.marginLeft }} >
           <HeaderCommon   />
